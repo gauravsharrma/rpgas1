009dac6709d35c526cf3c65eee55e822b3ce22e0
gauravsharrma <gauravsharrma00@gmail.com>
2025-06-28 00:50:47 +0400

    Update IconPack.tsx

--- components/IconPack.tsx
+++ components/IconPack.tsx
@@ -73,6 +73,19 @@ export const PhoneIcon: React.FC = () => (
     </svg>
 );
 
+// Shared Icons from other components
+export const LocationSolidIcon: React.FC<{ className?: string }> = ({ className = "h-5 w-5" }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 20 20" fill="currentColor">
+        <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 20l-4.95-5.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+    </svg>
+);
+
+export const WhatsAppIcon: React.FC<{ className?: string }> = ({ className = "w-6 h-6" }) => (
+    <svg aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" className={className}>
+        <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.89-5.451 0-9.887 4.434-9.889 9.884-.001 2.225.651 4.315 1.731 6.086l-.219.354-.925 3.375 3.414-.894.342-.089z" />
+    </svg>
+);
+
 // Added Icons
 export const BrochureIcon: React.FC = () => (
   <svg {...iconProps}><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.253v11.494m-5.25-11.494v11.494M17.25 6.253v11.494M3.75 6.253v11.494M20.25 12H3.75" /></svg>
